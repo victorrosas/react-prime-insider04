b645da652d1bdee70efe7acd4f2e670251419f6d
Victor Rosas <victor.rosas.96@hotmail.com>
2021-09-27 21:30:53 -0500

    first day

---
+++ src/components/Header/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  height: 70px;
+  flex-direction: row;
+  align-items: center;
+  padding-left: 14px;
+`;
+
+export const MenuButton = styled.TouchableOpacity`
+  height: 70px;
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const Title = styled.Text`
+  font-size: 30px;
+  color: #FFF;
+  font-weight: bold;
+  margin-left: 14px;
+`;
---
+++ src/pages/Movies/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function Movies() {
+  return(
+    <View>
+      <Text>Tela Movies</Text>
+    </View>
+  )
+}
+
+export default Movies;
